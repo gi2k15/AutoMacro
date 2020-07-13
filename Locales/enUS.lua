f8638061aedf4c1defdd1ae75d948d4bc69dd1a4
gi2k15 <gendouikari2015@gmail.com>
2020-07-13 11:44:34 -0300

    Initial migration to GitHub

---
+++ Locales/enUS.lua
@@ -0,0 +1,18 @@
+local L = LibStub("AceLocale-3.0"):NewLocale("AutoMacro", "enUS", true)
+
+L = L or {}
+--@localization(locale="enUS", format="lua__additive_table", same-key-is-true=true)@
+--@do-not-package@
+L["AutoMacro configuration"] = true
+L["Cannot create macros while in combat."] = true
+L["Creates spell macros with a single click."] = true
+L["Macro created successfully!"] = true
+L["Overwrite existing macro"] = true
+L["Replace the existing macro with the one you just created."] = true
+L["Right click for options"] = true
+L["Save macro in the General tab"] = true
+L["Save the macro in the General tab, making it available to other characters."] = true
+L["The %s tag is missing or incorrectly typed."] = true
+L["There is already a macro named %s."] = true
+L["Type below the macro you want to create with AutoMacro. Use %s where the name of the spell clicked should be replaced."] = true
+--@end-do-not-package@
---
+++ Locales/ruRU.lua
@@ -0,0 +1,18 @@
+local L = LibStub("AceLocale-3.0"):NewLocale("AutoMacro", "ruRU")
+
+L = L or {}
+--@localization(locale="ruRU", format="lua__additive_table", same-key-is-true=true)@
+--@do-not-package@
+L["AutoMacro configuration"] = "Настройка AutoMacro"
+L["Cannot create macros while in combat."] = "Нельзя создавать макросы во время боя."
+L["Creates spell macros with a single click."] = "Создает макросы заклинаний одним щелчком мыши."
+L["Macro created successfully!"] = "Макрос создан успешно!"
+L["Overwrite existing macro"] = "Перезаписать существующий макрос"
+L["Replace the existing macro with the one you just created."] = "Замените существующий макрос на только что созданный."
+L["Right click for options"] = "Щелкните правой кнопкой мыши для выбора параметров"
+L["Save macro in the General tab"] = "Сохраните макрос на вкладке Общие"
+L["Save the macro in the General tab, making it available to other characters."] = "Сохраните макрос на вкладке Общие, сделав его доступным для других персонажей."
+L["The %s tag is missing or incorrectly typed."] = "Тег %s отсутствует или неправильно набран."
+L["There is already a macro named %s."] = "Уже существует макрос с именем %s."
+L["Type below the macro you want to create with AutoMacro. Use %s where the name of the spell clicked should be replaced."] = "Введите ниже макрос, который вы хотите создать с помощью AutoMacro. Используйте %s, где имя нажатого заклинания должно быть заменено."
+--@end-do-not-package@
