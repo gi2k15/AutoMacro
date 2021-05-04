9f3a09aa7e4306525228d575e8ac9a61b5a0af99
Beviselynt <gendouikari2015@gmail.com>
2021-05-04 10:36:11 -0300

    Added Korean translation (thanks, netaras!)

---
+++ Locales/koKR.lua
@@ -0,0 +1,18 @@
+local L = LibStub("AceLocale-3.0"):NewLocale("AutoMacro", "koKR")
+
+L = L or {}
+--@localization(locale="koKR", format="lua_additive_table", same-key-is-true=true)@
+--@do-not-package@
+L["AutoMacro configuration"] = "AutoMacro 구성"
+L["Cannot create macros while in combat."] = "전투 중에는 매크로를 만들 수 없습니다."
+L["Creates spell macros with a single click."] = "클릭 한 번으로 주문 매크로를 만듭니다."
+L["Macro created successfully!"] = "매크로 생성에 성공했습니다!"
+L["Overwrite existing macro"] = "기존 매크로 덮어쓰기"
+L["Replace the existing macro with the one you just created."] = "기존 매크로를 방금 만든 거로 바꿉니다."
+L["Right click for options"] = "옵션은 우클릭"
+L["Save macro in the General tab"] = "일반 탭에 매크로 저장"
+L["Save the macro in the General tab, making it available to other characters."] = "일반 탭에 매크로를 저장하여 다른 캐릭터가 사용할 수 있도록 합니다."
+L["The %s tag is missing or incorrectly typed."] = "%s 태그가 없거나 잘못 입력했습니다."
+L["There is already a macro named %s."] = "%s|1이라는;라는; 매크로가 이미 있습니다."
+L["Type below the macro you want to create with AutoMacro. Use %s where the name of the spell clicked should be replaced."] = "AutoMacro로 만들려는 매크로를 아래에 입력하세요. 클릭한 주문의 이름을 대체해야 하는 곳에 %s|1을;를; 사용하세요."
+--@end-do-not-package@
